ace79be4584b996861fda996da4bff534192206a
Marcel Tanuri <marcel.tanuri@gmail.com>
2025-09-10 16:10:14 -0300

    fix down target

--- make/down.mk
+++ make/down.mk
@@ -3,3 +3,5 @@ down: ## Para e remove os contêineres, redes, volumes e imagens locais.
 	@docker compose down --volumes --rmi local
 	@rm	-f liferay/patching/*.zip
 	@rm	-rf liferay/config/.all
+	@rm	-rf elasticsearch-data
+	@rm	-rf liferay/document-library
