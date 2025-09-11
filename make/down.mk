0eb5e2c4786514a94100c7a9f95a47084c0b177e
Marcel Tanuri <marcel.tanuri@gmail.com>
2025-09-11 15:26:29 -0300

    fix down target

--- make/down.mk
+++ make/down.mk
@@ -1,10 +1,14 @@
 down: ## Para e remove os contêineres, redes, volumes e imagens locais.
+	@if [ ! -f liferay/.all-env ]; then \
+		echo "==> Criando arquivo liferay/.all-env (não existia)"; \
+		touch liferay/.all-env; \
+	fi
+
 	@echo "==> Derrubando containers, removendo volumes e imagens locais..."
 	@docker compose down --volumes --rmi local
-	@rm	-f liferay/patching/*.zip
+	@rm -f liferay/patching/*.zip
 	@rm -rf liferay/config/.all
 	@rm -rf liferay/config/.custom
-	@rm	-rf elasticsearch-data
-	@rm	-rf liferay/document-library
-	@rm	-rf liferay/deploy/*
-	@rm	-rf echo "" > liferay/.all-env
+	@rm -rf elasticsearch-data
+	@rm -rf liferay/document-library
+	@rm -rf liferay/deploy/*
