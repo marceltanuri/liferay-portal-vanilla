dc39e14258dc3a7ac3dd669a1ef618336d448b65
Marcel Tanuri <marcel.tanuri@gmail.com>
2025-09-16 15:39:47 -0300

    disable clearing patching

--- make/down.mk
+++ make/down.mk
@@ -6,7 +6,7 @@ down: ## Para e remove os contêineres, redes, volumes e imagens locais.
 
 	@echo "==> Derrubando containers, removendo volumes e imagens locais..."
 	@docker compose down --volumes --rmi local
-	@rm -f liferay/patching/*.zip
+#   @rm -f liferay/patching/*.zip
 	@rm -rf liferay/config/.all
 	@rm -rf liferay/config/.custom
 	@rm -rf elasticsearch-data/*
